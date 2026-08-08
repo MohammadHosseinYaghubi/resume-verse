f5e42e6b8820125ed24bdeba0af7c772b6f4db5b
unknown <mhossein.yaghubi20@gmail.com>
2026-08-09 00:38:46 +0330

    update auth

--- apps/accounts/services/auth.py
+++ apps/accounts/services/auth.py
@@ -1,5 +1,6 @@
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from apps.core_logging.services import log_event
+from apps.core_logging.constants import LogAction
 from apps.accounts.services.tokens import create_tokens
 
 
@@ -8,12 +9,22 @@ def login_user(user) -> dict:
     Generate JWT tokens for authenticated user.
     """
 
+    log_event(
+        action=LogAction.LOGIN,
+        message="User logged in.",
+        user=user,
+    )
     return create_tokens(user)
 
 
-def logout_user(refresh_token: str) -> None:
+def logout_user(*, user, refresh_token: str) -> None:
     """
     Blacklist refresh token.
     """
-
+  
+    log_event(
+        action=LogAction.LOGOUT,
+        message="User logged out.",
+        user=user,
+    )
     RefreshToken(refresh_token).blacklist()
